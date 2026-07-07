384fb8b99cf2e6de2efc7c0ccbe4b72bb031170f
Digbijay Singh <digbijaysingh766@gmail.com>
2026-07-07 14:04:25 +0530

    Added Django REST Framework

--- placement/settings.py
+++ placement/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'placeapp'
+    'placeapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
